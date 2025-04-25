81e73efb0703bed03683bd57a6535f374f71ba5d
helixedra <greencastle08@gmail.com>
2025-04-25 03:55:17 +0200

    fix auth5

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: `${process.env.NEXT_PUBLIC_APP_URL}`,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "img.clerk.com" },
